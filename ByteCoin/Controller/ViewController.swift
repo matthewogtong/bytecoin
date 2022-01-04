217a9e6b8d3a148657a90cc3f4cf70dac1f9be86
matthewogtong <ogtongm@gmail.com>
2022-01-04 12:33:51 -0500

    Successfully get PickerView to work along with printing out currently selected array item

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -29,7 +29,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.viewDidLoad()
         
         currencyPicker.dataSource = self
+        currencyPicker.delegate = self
         
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
     }
 
 
