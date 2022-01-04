af183da9d84207b0b21d8a886913cc64b4fe4665
matthewogtong <ogtongm@gmail.com>
2022-01-04 12:39:06 -0500

    Update pickerView method to pass the selected currency to the CoinManager via the getCointPrice method

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -38,7 +38,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(coinManager.currencyArray[row])
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
     }
 
 
