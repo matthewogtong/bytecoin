c8cd9a66f99e9afd8def47f7d77a3b02f554ad3e
matthewogtong <ogtongm@gmail.com>
2022-01-04 13:51:34 -0500

    Implement performRequest method using URLSession object to make network request and display JSON data from coinapi

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,12 +11,42 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "C76EAF0D-C79C-4A00-86EC-C47014728F8E"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-        print(currency)
+        
+        let url = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        performRequest(with: url)
+        
     }
     
+    func performRequest(with urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                
+                if error != nil {
+                    return
+                }
+                
+                if let safeData = data {
+                    let stringData = String(data: safeData, encoding: String.Encoding.utf8)
+                    
+                    guard let stringData = stringData else {
+                        return
+                    }
+                    
+                    print(stringData)
+                }
+                
+            }
+            task.resume()
+        }
+    }
 }
